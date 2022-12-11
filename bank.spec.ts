9e75173a7b9ef4428a85672753ec9deb911ddef5
coralieO <ouedraogocoralie@edu.devinci.fr>
2022-12-11 12:39:25 +0100

    when withdrawal give negative solde

--- bank.spec.ts
+++ bank.spec.ts
@@ -22,7 +22,13 @@ describe('withdrawal', () => {
     //     expect(withdrawal(0,500)).toBe(0)
     // })
     
-    it('should return 0 when solde = 0', () => {
-        expect(withdrawal(0,500)).toBe("error")
+    it('should return "your balance is insufficient" when solde = 0', () => {
+        expect(withdrawal(0,500)).toBe("Your balance is insufficient")
     })
+    it('should return "Cannot withdraw" when solde - montant < 0', () => {
+        expect(withdrawal(50,100)).toBe("Can't withdraw")
+    })
+    // it('should return 250.5 wh', () => {
+    //     expect(withdrawal(300.5,50)).toBe(250.5)
+    // })
 })
--- withdrawal.ts
+++ withdrawal.ts
@@ -1,6 +1,7 @@
 export const withdrawal = (solde: number, montant: number): number | string => {
 
-    if(solde === 0) return "error";
+    if(solde === 0) return "Your balance is insufficient";
+    if(solde - montant < 0) return "Can't withdraw";
     // if(solde === 0){
     //     // let msg = 'error = insufficient balance' 
     //     // console.log(msg)
